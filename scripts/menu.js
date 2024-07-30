69b7f4ceb1c22a4176e18666aa65a2479d2369c9
vindhyaveera <vindhyadevi12@gmail.com>
2024-07-30 13:35:14 +0530

    success

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,10 +9,10 @@ const profilecontent = document.querySelector(".profilecontent");
 
 // Toggle main menu on large screens
 menuicon.addEventListener("click", function () {
-  if (window.innerWidth > 600) {
+  
     menuitems.classList.add("menuitemsclick");
-    headmenuicon.style.display = "none"; // Hide the menu icon
-  }
+    // headmenuicon.style.display = "none"; // Hide the menu icon
+  
 });
 // menuicon.addEventListener("click", function () {
 //   mainmenusmall.classList.toggle("show");
@@ -27,15 +27,13 @@ menuicon.addEventListener("click", function () {
 
 // Toggle main menu on small screens
 menuicon.addEventListener("click", function () {
-  if (window.innerWidth <= 600) {
-    mainmenusmall.classList.toggle("show");
-    profilecontent.classList.toggle("hide-profilecontent");
-    headmenuicon.style.display = "none"; // Hide the menu icon
+    mainmenusmall.classList.add("show");
+    // headmenuicon.style.display = "none"; // Hide the menu icon
   }
-});
+);
+
 
 menuIconclose.addEventListener("click", function () {
   mainmenusmall.classList.remove("show");
-  profilecontent.classList.remove("hide-profilecontent");
-  headmenuicon.style.display = "flex"; // Show the menu icon again
+  // headmenuicon.style.display = "flex"; // Show the menu icon again
 });
